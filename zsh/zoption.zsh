db19192fbdcdf06312b82a2c83dfa4f20ea2781e
Ultra-Code <mega.alpha100@gmail.com>
2021-10-02 18:35:59 +0000

    Improve and remove redundant history options
    Add option for glob completion and correcting commands
    
    Comment out options that I don't find very useful

--- zsh/zoption.zsh
+++ zsh/zoption.zsh
@@ -1,10 +1,16 @@
 #Shell options
-setopt HIST_IGNORE_ALL_DUPS # Do not write a duplicate event to the history file.
-setopt HIST_SAVE_NO_DUPS #older commands that duplicate newer ones are omitted.
-setopt HIST_IGNORE_SPACE # ignore commands that start with space
-setopt HIST_REDUCE_BLANKS #Remove superfluous blanks from each command line being added to the history list
-setopt SHARE_HISTORY     # Enable shells to read and write to the most recent history
-setopt APPEND_HISTORY #zsh sessions will append their history list to the history file, rather than replace it
+# Do not write a duplicate event to the history file.
+setopt HIST_IGNORE_ALL_DUPS
+#older commands that duplicate newer ones are omitted when writing out from the
+#internal to external history
+setopt HIST_SAVE_NO_DUPS
+# ignore commands that start with space
+# Enable shells to read and write to the most recent history
+setopt HIST_IGNORE_SPACE
+#Remove superfluous blanks from each command line being added to the history list
+setopt HIST_REDUCE_BLANKS
+#use a single, shared history file across the sessions and append to it rather than overwrite
+setopt SHARE_HISTORY
 
 #Make cd push the old directory onto the directory stack
 setopt AUTO_PUSHD
@@ -24,15 +30,23 @@ setopt PROMPT_SUBST
 
 #Completion configurations
 #
-unsetopt MENU_COMPLETE
-unsetopt FLOWCONTROL
 #completion is done from both ends of the word
 setopt COMPLETE_IN_WORD
+#if set hit the tab key will list possible completions, but not substitute them in the
+#command prompt
+setopt GLOB_COMPLETE
+#Try  to correct the spelling of commands
+setopt CORRECT
+#unsetopt MENU_COMPLETE
+#unsetopt FLOWCONTROL
 #Automatically use menu completion after the second consecutive request for
 #completion
-setopt AUTO_MENU
+#setopt AUTO_MENU
 #move cursor to the end of the word if completion was started with it in within
 #word
-setopt ALWAYS_TO_END
+#setopt ALWAYS_TO_END
+
+#MISC
 # Don't send SIGHUP to background processes when the shell exits.
 setopt NOHUP
+
