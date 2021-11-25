979ff9ba82e157c28d6fe49f677597ac670293a1
Ultra-Code <mega.alpha100@gmail.com>
2021-11-25 17:29:07 +0000

    Add keybinding for switching to interactive mode in menu completion

--- zsh/zkeybind.zsh
+++ zsh/zkeybind.zsh
@@ -11,6 +11,9 @@ bindkey -M menuselect 'k' vi-up-line-or-history
 bindkey -M menuselect 'l' vi-forward-char
 bindkey -M menuselect 'j' vi-down-line-or-history
 
+#configure CTRL+x i to switch to the interactive mode in the completion menu
+bindkey -M menuselect '^xi' vi-insert
+
 #use vim key bindings kj for scrolling history in vim command mode
 bindkey -M vicmd 'k' vi-up-line-or-history
 bindkey -M vicmd 'j' vi-down-line-or-history
