24a941282f77210e62e4a18e50e6bac9cc36aa54
Ultra-Code <mega.alpha100@gmail.com>
2022-03-29 03:11:17 +0000

    Improve script and add binding to open file found by rg in fzf

--- zsh/rgf.zsh
+++ zsh/rgf.zsh
@@ -6,18 +6,16 @@
 # 3. Open the file in Vim
 RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
 INITIAL_QUERY="${*:-}"
-IFS=: read -ra selected < <(
-  FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" \
-  fzf --ansi \
-      --color "hl:-1:underline,hl+:-1:underline:reverse" \
-      --disabled --query "$INITIAL_QUERY" \
-      --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true " \
-      --bind "ctrl-f:unbind(change)+change-prompt(2. fzf> )+enable-search+clear-query" \
-      --prompt '1. ripgrep> ' \
-      --delimiter : \
-      --preview 'bat --color=always {1} --highlight-line {2}' \
-      --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
-      # UNTILE https://github.com/junegunn/fzf/issues/2752 is fixed  .ie rebinding of the change event is allowed
-      # --bind "ctrl-r:unbind(ctrl-f)+change-prompt(1. ripgrep> )+disable-search+clear-query,change:reload(sleep 0.1; $RG_PREFIX {q} || true)" \
-)
-[ -n "${selected[0]}" ] && $EDITOR "${selected[0]}" "+${selected[1]}"
+FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" \
+fzf --ansi \
+  --color "hl:-1:underline,hl+:-1:underline:reverse" \
+  --disabled --query "$INITIAL_QUERY" \
+  --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true " \
+  --bind "ctrl-f:unbind(change)+change-prompt(2. fzf> )+enable-search+clear-query" \
+  --bind "enter:execute:nvim {1}" \
+  --prompt '1. ripgrep> ' \
+  --delimiter : \
+  --preview 'bat --color=always {1} --highlight-line {2}' \
+  --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
+  # UNTILE https://github.com/junegunn/fzf/issues/2752 is fixed  .ie rebinding of the change event is allowed
+  # --bind "ctrl-r:unbind(ctrl-f)+change-prompt(1. ripgrep> )+disable-search+clear-query,change:reload(sleep 0.1; $RG_PREFIX {q} || true)" \
