c0a0e83126d84101885d933510237aba493e9f0b
Ultra-Code <mega.alpha100@gmail.com>
2022-04-08 00:37:43 +0000

    Fix up rgf to use the new rebind action
    This help to switch between ctrl-r and ctrl-f keybinding repeatedly

--- zsh/rgf.zsh
+++ zsh/rgf.zsh
@@ -5,17 +5,17 @@
 #    * Press ctrl-f to switch to fzf-only filtering
 # 3. Open the file in Vim
 RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
-INITIAL_QUERY="${*:-}"
-FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q "$INITIAL_QUERY")" \
-fzf --ansi \
-  --color "hl:-1:underline,hl+:-1:underline:reverse" \
-  --disabled --query "$INITIAL_QUERY" \
-  --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true " \
-  --bind "ctrl-f:unbind(change)+change-prompt(2. fzf> )+enable-search+clear-query" \
-  --bind "enter:execute:nvim {1}" \
-  --prompt '1. ripgrep> ' \
-  --delimiter : \
-  --preview 'bat --color=always {1} --highlight-line {2}' \
-  --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
-  # UNTILE https://github.com/junegunn/fzf/issues/2752 is fixed  .ie rebinding of the change event is allowed
-  # --bind "ctrl-r:unbind(ctrl-f)+change-prompt(1. ripgrep> )+disable-search+clear-query,change:reload(sleep 0.1; $RG_PREFIX {q} || true)" \
+INITIAL_QUERY=${*:-}
+  FZF_DEFAULT_COMMAND="$RG_PREFIX $(printf %q $INITIAL_QUERY)" \
+  fzf --ansi \
+      --color "hl:-1:underline,hl+:-1:underline:reverse" \
+      --disabled --query "$INITIAL_QUERY" \
+      --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+      --bind "ctrl-f:unbind(change,ctrl-f)+change-prompt(2. fzf> )+clear-query+enable-search+rebind(ctrl-r)" \
+      --bind "ctrl-r:unbind(ctrl-r)+change-prompt(1. ripgrep> )+disable-search+reload($RG_PREFIX {q} || true)+rebind(change,ctrl-f)" \
+      --bind "enter:execute:nvim {1}" \
+      --prompt '1. ripgrep> ' \
+      --delimiter : \
+      --preview 'bat --color=always {1} --highlight-line {2}' \
+      --preview-window 'up,60%,border-bottom,+{2}+3/3,~3'
+      # --header 'ctrl-r (ripgrep) ╱ ctrl-f (fzf)╱' \
