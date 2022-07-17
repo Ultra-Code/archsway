83be82efc2d0347d1f30b19fefd922cce64da89b
Ultra-Code <mega.alpha100@gmail.com>
2022-07-17 15:13:12 +0000

    Running swaynag in the background prevent systemd from killing it
    When runned in the foreground for a long time without recieving a input
    
    systemd by default kills it,this default behaviour allows for consecutive
    
    swaynag updates since the previous is killed
    
    This implies that it is prefered to play the low battery sound before running swaynag
    
    Also for when battery is lower than 4% let udev deal with powermanagement by doing nothing

--- archlinux/sway/low-battery/low-battery.zsh
+++ archlinux/sway/low-battery/low-battery.zsh
@@ -6,38 +6,43 @@ BATTERY_CAPACITY_PATH='/sys/class/power_supply/BAT0/capacity'
 BATTERY_STATUS=$(cat $POWERSUPPLY_PATH)
 BATTERY_CAPACITY=$(cat $BATTERY_CAPACITY_PATH)
 
-if [[ $BATTERY_CAPACITY -gt 25 ]]
+if [[ $BATTERY_CAPACITY -gt 25 || $BATTERY_STATUS == 'Charging' ]]
 then
+    return
+
+elif [[ $BATTERY_CAPACITY -lt 4  &&  $BATTERY_STATUS == 'Discharging' ]]
+then
+    # at this point the udev rule ../../powersaving/low-battery.rules take over
     return
 
 elif [[ $BATTERY_CAPACITY -lt 7  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
    local CRITICAL_BATTERY_SOUND='/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga'
+    paplay $CRITICAL_BATTERY_SOUND
 
     swaynag -t error -m "Battery Critical : About to shutdown connect charger Battery level is ${BATTERY_CAPACITY}%!" \
         -b 'Hybrid-sleep' 'systemctl hybrid-sleep' -b 'Hibernate' 'systemctl hibernate' \
-        -s 'NO' &
+        -s 'NO'
 
-    paplay $CRITICAL_BATTERY_SOUND
 
 elif [[ $BATTERY_CAPACITY -lt 15  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
    local VERY_LOW_BATTERY_SOUND='/usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga'
+    paplay $VERY_LOW_BATTERY_SOUND
 
     swaynag -t warning -m "Very Low Battery : About to shutdown connect charger Battery level is ${BATTERY_CAPACITY}%!" \
         -b 'Hybrid-sleep' 'systemctl hybrid-sleep' -b 'Hibernate' 'systemctl hibernate' \
-        -s 'NO' &
+        -s 'NO'
 
-    paplay $VERY_LOW_BATTERY_SOUND
 
 elif [[ $BATTERY_CAPACITY -lt 20  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
     local LOW_BATTERY_SOUND='/usr/share/sounds/freedesktop/stereo/suspend-error.oga'
+    paplay $LOW_BATTERY_SOUND
 
     swaynag -t warning -m "Low Battery : Battery level is ${BATTERY_CAPACITY}%!" \
-        -b 'Hybrid-sleep' 'systemctl hybrid-sleep' -s 'NO' &
+        -b 'Hybrid-sleep' 'systemctl hybrid-sleep' -s 'NO'
 
-    paplay $LOW_BATTERY_SOUND
 
 elif [[ $BATTERY_CAPACITY -lt 25  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
