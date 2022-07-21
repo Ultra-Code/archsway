8f1df1a75fc37e4d75437fc682b3dfe74799b7a1
Ultra-Code <mega.alpha100@gmail.com>
2022-07-21 17:14:40 +0000

    Disable XHC wakeup and hybrid-sleep at 1-9%
    sync low-battery.zsh with low-battery.rules
    
    update history

--- sway/low-battery/low-battery.zsh
+++ sway/low-battery/low-battery.zsh
@@ -10,43 +10,37 @@ if [[ $BATTERY_CAPACITY -gt 25 || $BATTERY_STATUS == 'Charging' ]]
 then
     return
 
-elif [[ $BATTERY_CAPACITY -lt 4  &&  $BATTERY_STATUS == 'Discharging' ]]
+elif [[ $BATTERY_CAPACITY -lt 10  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
     # at this point the udev rule ../../powersaving/low-battery.rules take over
     return
 
-elif [[ $BATTERY_CAPACITY -lt 7  &&  $BATTERY_STATUS == 'Discharging' ]]
+elif [[ $BATTERY_CAPACITY -lt 16  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
    local CRITICAL_BATTERY_SOUND='/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga'
     paplay $CRITICAL_BATTERY_SOUND
 
-    swaynag -t error -m "Battery Critical : About to shutdown connect charger Battery level is ${BATTERY_CAPACITY}%!" \
+    swaynag -t error -m "Battery Critical : About To Shutdown Connect Charger Immediately : Battery Level is ${BATTERY_CAPACITY}%!" \
         -b 'Hybrid-sleep' 'systemctl hybrid-sleep' -b 'Hibernate' 'systemctl hibernate' \
         -s 'NO'
 
 
-elif [[ $BATTERY_CAPACITY -lt 15  &&  $BATTERY_STATUS == 'Discharging' ]]
+elif [[ $BATTERY_CAPACITY -lt 21  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
    local VERY_LOW_BATTERY_SOUND='/usr/share/sounds/freedesktop/stereo/phone-outgoing-busy.oga'
     paplay $VERY_LOW_BATTERY_SOUND
 
-    swaynag -t warning -m "Very Low Battery : About to shutdown connect charger Battery level is ${BATTERY_CAPACITY}%!" \
+    swaynag -t warning -m "Very Low Battery : Connect Charger : Battery Level is ${BATTERY_CAPACITY}%!" \
         -b 'Hybrid-sleep' 'systemctl hybrid-sleep' -b 'Hibernate' 'systemctl hibernate' \
         -s 'NO'
 
 
-elif [[ $BATTERY_CAPACITY -lt 20  &&  $BATTERY_STATUS == 'Discharging' ]]
+elif [[ $BATTERY_CAPACITY -lt 25  &&  $BATTERY_STATUS == 'Discharging' ]]
 then
     local LOW_BATTERY_SOUND='/usr/share/sounds/freedesktop/stereo/suspend-error.oga'
     paplay $LOW_BATTERY_SOUND
 
-    swaynag -t warning -m "Low Battery : Battery level is ${BATTERY_CAPACITY}%!" \
+    swaynag -t warning -m "Low Battery : Battery Level is ${BATTERY_CAPACITY}%!" \
         -b 'Hybrid-sleep' 'systemctl hybrid-sleep' -s 'NO'
-
-
-elif [[ $BATTERY_CAPACITY -lt 25  &&  $BATTERY_STATUS == 'Discharging' ]]
-then
-    swaynag -t warning -m "Low Battery : Battery level is ${BATTERY_CAPACITY}%!" \
-        -s 'OK'
 
 fi
