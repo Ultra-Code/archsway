cacec511ab398ca7f282a0be6ee4241710dd4542
Ultra-Code <mega.alpha100@gmail.com>
2023-03-04 23:39:53 +0000

    sway(launcher): overhaul and revamp of launcher
    add error checking for dependencies and failed commands
    
    add ability to use enter for execution of selected commands
    in both executable and desktop modes
    
    replace external commands with zsh expansions(modifies and flags)

--- sway/launcher.zsh
+++ sway/launcher.zsh
@@ -1,9 +1,72 @@
-#unbind isn't evaluated immediately until an event or key stroke is executed and rebind is used to bind after unbinding
-FZF_DEFAULT_COMMAND='find /usr/share/applications -name "*.desktop" | xargs basename -s ".desktop"' \
-        fzf --no-multi --height 100% --layout reverse-list --margin 3% --padding 3% --info hidden \
-        --bind 'ctrl-d:reload(sleep 0.01;find /usr/share/applications -name "*.desktop" | xargs basename -s ".desktop")+change-prompt(desktop > )+rebind(enter)+unbind(alt-enter)' \
-        --bind 'enter:execute(swaymsg -t command exec gtk-launch {})+abort' \
-        --bind 'ctrl-e:reload(printenv PATH | xargs -d : -I {} -r find -L {} -maxdepth 1 -type f -executable -printf "%f\n" 2>/dev/null| sort -u)+change-prompt(executable > )+rebind(alt-enter)+unbind(enter)' \
-        --bind 'alt-enter:execute(swaymsg -t command exec kitty {})+abort' \
-        --prompt 'desktop > ' \
-        --header-first --header "					sway app launcher"
+#!/bin/zsh
+readonly name=${ZSH_ARGZERO:t} # or ${(%):-%1N};
+
+if ! which fzf &> /dev/null; then
+    printf "error: fzf is required to use %s.\n" ${name} >&2
+    exit 3
+fi
+
+declare user_input
+if ! user_input="$(mktemp --tmpdir "${name}.input.XXXXXXXXX")";then
+    print 'error: Failed to create tmp file. $TMPDIR (or /tmp if $TMPDIR is unset) may not be writable.\n' >&2
+    exit 4
+fi
+
+#https://unix.stackexchange.com/questions/710366/is-there-a-zsh-equivalent-of-bash-builtin-readarray
+#https://www.reddit.com/r/zsh/comments/tt6gm8/why_doesnt_zsh_have_an_equivalent_of_bashs/
+readonly HELP_MSG=("${(@)${(f)$(<< EOF
+launcher help\n
+
+ctrl-d       desktop mode (default)\n
+ctrl-e       exe mode\n
+
+Enter       Confirm selection\n
+
+?           Help (this page)\n
+ESC         Quit\n
+EOF
+)}}")
+
+readonly DESKTOP_FILES=("${(f)$(find /usr/share/applications -name '*.desktop')}")
+#remove directory path
+readonly DESKTOP_FILES_LIST=(${DESKTOP_FILES:t})
+#remove file extension
+readonly DESKTOP_NAMES=(${DESKTOP_FILES_LIST:r})
+
+#split on `:`
+readonly EXECUTABLE_PATH=("${(s.:.)PATH}")
+#find executable files in list and separate them based on `\n`
+readonly EXE_LIST=("${(f)$(find -L ${EXECUTABLE_PATH} -maxdepth 1 -type f -executable)}")
+#select the base names of only unique executable files
+readonly UNIQ_EXE_NAMES=(${(u)EXE_LIST:t})
+
+readonly DEFAULT_COMMAND="print -l $DESKTOP_NAMES"
+readonly ALT_COMMAND="print -l $UNIQ_EXE_NAMES"
+readonly HELP="print -l '$HELP_MSG'"
+
+echo "desktop" > $user_input &&
+    SHELL="/bin/zsh" \
+    FZF_DEFAULT_COMMAND="$DEFAULT_COMMAND" \
+    fzf \
+    --no-multi \
+    --height 100% \
+    --layout reverse-list \
+    --marker=' ' \
+    --margin 3% \
+    --padding 3% \
+    --info hidden \
+    --header "Press ? for help or ESC to quit" \
+    --prompt "desktop > " \
+    --bind "enter:abort+execute:
+        if grep -q 'desktop' $user_input; then
+            swaymsg -t command exec gtk-launch {}
+        else
+            swaymsg -t command exec kitty {}
+        fi
+    " \
+    --bind "ctrl-d:unbind(ctrl-d)+reload($DEFAULT_COMMAND)+change-prompt(desktop > )+execute-silent(echo 'desktop' > '$user_input')+rebind(ctrl-e)" \
+    --bind "ctrl-e:unbind(ctrl-e)+reload($ALT_COMMAND)+change-prompt(executable >)+execute-silent(echo 'terminal' > '$user_input')+rebind(ctrl-d)" \
+    --bind "?:preview($HELP)" \
+    --preview-window "bottom,40%"
+
+rm -f ${user_input} &> /dev/null
