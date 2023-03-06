8e37495c37dd63374b251d68a12a17b14be7cd18
Ultra-Code <mega.alpha100@gmail.com>
2023-03-06 00:20:44 +0000

    sway(launcher): futher improve launcher script
    remove the need for newline characters in HELP_MSG
    use the (F) expression flag to print HELP_MSG
    
    read the $user_input with zsh's $(<file) statement
    
    combine the type parameter expression steps use to
    generate DESKTOP_NAMES and UNIQ_EXE_NAMES into one

--- sway/launcher.zsh
+++ sway/launcher.zsh
@@ -14,35 +14,38 @@ fi
 
 #https://unix.stackexchange.com/questions/710366/is-there-a-zsh-equivalent-of-bash-builtin-readarray
 #https://www.reddit.com/r/zsh/comments/tt6gm8/why_doesnt_zsh_have_an_equivalent_of_bashs/
-readonly HELP_MSG=("${(@)${(f)$(<< EOF
-launcher help\n
+readonly HELP_MSG=("${(@f)$(<< EOF
+    launcher help
 
-ctrl-d       desktop mode (default)\n
-ctrl-e       exe mode\n
+    ctrl-d       desktop mode (default)
+    ctrl-e       exe mode
 
-Enter       Confirm selection\n
+    Enter       Confirm selection
 
-?           Help (this page)\n
-ESC         Quit\n
+    ?           Help (this page)
+    ESC         Quit
 EOF
-)}}")
+)}")
 
-readonly DESKTOP_FILES=("${(f)$(find /usr/share/applications -name '*.desktop')}")
-#remove directory path
-readonly DESKTOP_FILES_LIST=(${DESKTOP_FILES:t})
-#remove file extension
-readonly DESKTOP_NAMES=(${DESKTOP_FILES_LIST:r})
+# readonly DESKTOP_FILES=("${(f)$(find /usr/share/applications -name '*.desktop')}")
+# #remove directory path
+# readonly DESKTOP_FILES_LIST=(${DESKTOP_FILES:t})
+# #remove file extension
+# readonly DESKTOP_NAMES=(${DESKTOP_FILES_LIST:r})
+readonly DESKTOP_NAMES=(${${"${(f)$(find /usr/share/applications -name '*.desktop')}":t}:r})
 
 #split on `:`
-readonly EXECUTABLE_PATH=("${(s.:.)PATH}")
-#find executable files in list and separate them based on `\n`
-readonly EXE_LIST=("${(f)$(find -L ${EXECUTABLE_PATH} -maxdepth 1 -type f -executable)}")
-#select the base names of only unique executable files
-readonly UNIQ_EXE_NAMES=(${(u)EXE_LIST:t})
+# readonly EXECUTABLE_PATH=("${(s.:.)PATH}")
+# #find executable files in list and separate them based on `\n`
+# readonly EXE_LIST=("${(f)$(find -L ${EXECUTABLE_PATH} -maxdepth 1 -type f -executable)}")
+# #select the base names of only unique executable files
+# readonly UNIQ_EXE_NAMES=(${(u)EXE_LIST:t})
+readonly UNIQ_EXE_NAMES=(${(u)"${(f)$(find -L "${(s.:.)PATH}" -maxdepth 1 -type f -executable)}":t})
 
 readonly DEFAULT_COMMAND="print -l $DESKTOP_NAMES"
 readonly ALT_COMMAND="print -l $UNIQ_EXE_NAMES"
-readonly HELP="print -l '$HELP_MSG'"
+#print lines in array with newlines joined to each element
+readonly HELP="print -l '${(F)HELP_MSG}'"
 
 echo "desktop" > $user_input &&
     SHELL="/bin/zsh" \
@@ -51,22 +54,22 @@ echo "desktop" > $user_input &&
     --no-multi \
     --height 100% \
     --layout reverse-list \
-    --marker=' ' \
     --margin 3% \
     --padding 3% \
     --info hidden \
     --header "Press ? for help or ESC to quit" \
     --prompt "desktop > " \
-    --bind "enter:abort+execute:
-        if grep -q 'desktop' $user_input; then
+    --bind "enter:execute<
+        state=\$(<$user_input)
+        if [[ '$state' == 'desktop' ]]; then
             swaymsg -t command exec gtk-launch {}
         else
             swaymsg -t command exec kitty {}
         fi
-    " \
+    >+abort" \
     --bind "ctrl-d:unbind(ctrl-d)+reload($DEFAULT_COMMAND)+change-prompt(desktop > )+execute-silent(echo 'desktop' > '$user_input')+rebind(ctrl-e)" \
     --bind "ctrl-e:unbind(ctrl-e)+reload($ALT_COMMAND)+change-prompt(executable >)+execute-silent(echo 'terminal' > '$user_input')+rebind(ctrl-d)" \
     --bind "?:preview($HELP)" \
     --preview-window "bottom,40%"
 
-rm -f ${user_input} &> /dev/null
+rm -f $user_input &> /dev/null
