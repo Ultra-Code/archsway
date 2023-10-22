1bd45c32b5ab992f835aab3dd08c30d2cde43882
Ultra-Code <mega.alpha100@gmail.com>
2023-10-22 16:31:29 +0000

    lf: overhaul of lf
    Enable icons and colors
    Use dim attribute for preview pane
    Enable extract command
    Add mapping for making and deleting directories
    
    Preview overhaul
    Use md5sum for checksum and generating file cache since it is simpler,
    Requires less resource and sufficient for this case
    
    Prefer only extension for matching over mime type
    Rename and better organize preview handlers
    Enable color always for bat and use plain mode
    
    lfcd
    add lfcd zsh function
    use lf as the default file manager
    source lfcd in zshrc

--- fm/lf/preview.zsh
+++ fm/lf/preview.zsh
@@ -11,20 +11,18 @@ FILE_EXT=${FILE:e}
 readonly FILE_EXT=${FILE_EXT:l}
 readonly CACHE_PATH="${TMPDIR:-/tmp}/lf_cache"
 
-#mime type of file
-readonly MIMETYPE=$(file --dereference --brief --mime-type "$FILE")
-
 #my improvement on https://raw.githubusercontent.com/duganchen/kitty-pistol-previewer/main/vidthumb
 function cache(){
     if (( $# < 2 )); then
-    print "usage: $0 CACHE_PATH File" >&2
-    exit -1
-  fi
+        print "usage: $0 CACHE_PATH File" >&2
+        exit -1
+    fi
+
     if  [[  ! -d $1 || ! -f $2 ]]; then
         exit 1
     fi
 
-    printf '%s/%s' $1 "$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$2")" | b2sum | cut -d ' ' -f1)"
+    printf '%s/%s' $1 "$(stat --printf '%n\0%i\0%F\0%s\0%W\0%Y' -- "$(readlink -f "$2")" | md5sum | cut -d ' ' -f1)"
 }
 
 readonly CACHED_FILE=$(cache $CACHE_PATH "$FILE")
@@ -39,39 +37,33 @@ function isImageNew {
 
 function preview(){
     if (( $# < 1 )); then
-    print "usage: $0 File" >&2
-    exit -1
-  fi
-  #If the previewer returns a non-zero exit code, then the preview cache for the given file is disabled.
-  #This means that if the file is selected in the future, the previewer is called once again.
-  #The cleaner is only called if previewing is enabled, the previewer is set, and the previously selected file had its preview cache disabled
+        print "usage: $0 File" >&2
+        exit -1
+    fi
+    #If the previewer returns a non-zero exit code, then the preview cache for the given file is disabled.
+    #This means that if the file is selected in the future, the previewer is called once again.
+    #The cleaner is only called if previewing is enabled, the previewer is set, and the previously selected file had its preview cache disabled
     if [[ -n $WAYLAND_DISPLAY ]]
     then
-            kitten icat --transfer-mode=file --stdin=no --place "${WIDTH}x${HEIGHT}@${XOFFSET}x${YOFFSET}" "$1" < /dev/null > /dev/tty && exit 1;
+        kitten icat --transfer-mode=file --stdin=no --place "${WIDTH}x${HEIGHT}@${XOFFSET}x${YOFFSET}" "$1" < /dev/null > /dev/tty && exit 1;
     fi
 }
 
 
-function handle_image() {
-
-    local mimetype=$1
-    case "$mimetype" in
-        ## SVG
-        # image/svg+xml|image/svg)
-        #     convert -- "${FILE}" $CACHED_FILE && exit 6
-        #     exit 1;;
-
+function handle_media() {
+    local ext=$1
+    case "$ext" in
         ## Image
-        image/*)
-        # if exists convert; then
-        #                convert "$3" -flatten -resize "$NNN_PREVIEWWIDTH"x"$NNN_PREVIEWHEIGHT"\> "$NNN_PREVIEWDIR/$3.jpg"
-        #            else
-                # chafa -f sixel $FILE
-                preview $FILE
-                ;;
+        png|jpg)
+            # if exists convert; then
+            #   convert "$3" -flatten -resize "$NNN_PREVIEWWIDTH"x"$NNN_PREVIEWHEIGHT"\> "$NNN_PREVIEWDIR/$3.jpg"
+            # else
+            # chafa -f sixel $FILE
+            preview $FILE
+        ;;
 
         ## Video
-        video/*)
+        mkv|webm|mp4|avi)
             # Thumbnail
             if [[ $(isImageNew) == "true" ]];then
                 lf -remote "send $id echo 'caching'"
@@ -80,10 +72,27 @@ function handle_image() {
 
             preview $CACHED_FILE
 
-            exit 1;;
+            exit 1
+        ;;
 
+        ## SVG
+        # image/svg+xml|image/svg)
+        #     convert -- "${FILE}" $CACHED_FILE && exit 6
+        #     exit 1;;
+
+        ## Video and audio
+        # video/* | audio/*)
+        #     mediainfo "${FILE}" || \
+        #     exiftool "${FILE}" || exit $?;;
+    esac
+}
+
+
+function handle_documents() {
+    local ext=$1
+    case "$ext" in
         ## PDF
-        application/pdf)
+        pdf)
             if [[ $(isImageNew) == "true" ]];then
             pdftoppm -f 1 -l 1 \
                      -singlefile \
@@ -96,25 +105,23 @@ function handle_image() {
 
                 exit 1;;
 
-        # */*office*|*/*document*)
-        #     if [[ $(isImageNew) == "true" ]];then
-        #     #image is store in the cache dir with same name as FILE
-        #         libreoffice --convert-to jpg "$FILE" --outdir ${CACHE_PATH:A} &>/dev/null
-        #     #get the CACHED_FILE's dirname and append the file without its previous extension since its now jpeg so
-        #     #instead append .jpg and move to the CACHED_FILE
-        #     local current_img="${CACHE_PATH:A}/${${FILE:t}%.*}.jpg"
-        #
-        #         mv "$current_img" "$CACHED_FILE"
-        #     fi
-        #
-        #     preview $CACHED_FILE
-        #     ;;
-        #
+        odf|doc|docx)
+            if [[ $(isImageNew) == "true" ]];then
+            #image is store in the cache dir with same name as FILE
+                libreoffice --convert-to jpg "$FILE" --outdir ${CACHE_PATH:A} &>/dev/null
+            #get the CACHED_FILE's dirname and append the file without its previous extension since its now jpeg so
+            #instead append .jpg and move to the CACHED_FILE
+            local current_img="${CACHE_PATH:A}/${${FILE:t}%.*}.jpg"
+
+                mv "$current_img" "$CACHED_FILE"
+            fi
+
+            preview $CACHED_FILE
+        ;;
+
 
         ## ePub, MOBI, FB2 (using Calibre)
-        application/epub+zip|application/x-mobipocket-ebook|\
-        application/x-fictionbook+xml)
-
+        epub|mobi|fb2)
             if [[ $(isImageNew) == "true" ]];then
             gnome-epub-thumbnailer "${FILE}" $CACHED_FILE  || \
             gnome-mobi-thumbnailer "${FILE}" $CACHED_FILE  || \
@@ -124,23 +131,31 @@ function handle_image() {
             fi
 
             preview $CACHED_FILE
-            exit 1;;
+            exit 1
+        ;;
 
-    esac
+        ## Text | md
+        txt|md)
+            ## Syntax highlight
+            bat --color=always --paging=never --plain --terminal-width $WIDTH -- "${FILE}" || exit $?
+        ;;
 
-}
+        ## JSON
+        json)
+            bat --color=always --language=json --paging=never --plain "$FILE" || \
+            jq --color-output . "${FILE}" || \
+            python -m json.tool -- "${FILE}" || exit $?
+        ;;
 
-function handle_mime() {
-
-    local mimetype=$1
-    case "${mimetype}" in
-        ## RTF and DOC
-        # text/rtf|*msword)
-        #     ## Preview as text conversion
-        #     ## note: catdoc does not always work for .doc files
-        #     ## catdoc: http://www.wagner.pp.ru/~vitus/software/catdoc/
-        #     catdoc -- "${FILE}" && exit 5
-        #     exit 1;;
+        ## HTML
+        htm|html|xhtml)
+            ## Preview as text conversion
+            bat -l html -- "$FILE" || \
+            lynx -dump -- "${FILE}" || \
+            elinks -dump "${FILE}" || \
+            w3m -dump "${FILE}" || \
+            pandoc -s -t markdown -- "${FILE}" || exit $?
+        ;;
 
         ## DOCX, ePub, FB2 (using markdown)
         ## You might want to remove "|epub" and/or "|fb2" below if you have
@@ -148,48 +163,6 @@ function handle_mime() {
         # *wordprocessingml.document|*/epub+zip|*/x-fictionbook+xml)
         #     ## Preview as markdown conversion
         #     pandoc -s -t markdown -- "${FILE}" || exit $?;;
-
-        ## XLS
-        # *ms-excel)
-        #     ## Preview as csv conversion
-        #     ## xls2csv comes with catdoc:
-        #     ##   http://www.wagner.pp.ru/~vitus/software/catdoc/
-        #     xls2csv -- "${FILE}" || exit $?;;
-
-        ## Text | md
-        text/*)
-            ## Syntax highlight
-            bat --color=always --style="plain" --terminal-width $WIDTH --pager never -- "${FILE}" || exit $?;;
-
-        ## Image
-        # image/*)
-        #     ## Preview as text conversion
-        #     # img2txt --gamma=0.6 --width=$WIDTH -- "${FILE}" && exit 4
-        #     exiftool "${FILE}" || exit $?;;
-
-        ## Video and audio
-        # video/* | audio/*)
-        #     mediainfo "${FILE}" || \
-        #     exiftool "${FILE}" || exit $?;;
-    esac
-}
-
-function handle_extension() {
-
-    local ext=$1
-    case "${ext}" in
-        ## Archive
-        a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
-        rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
-            bsdtar --list --file "${FILE}" || \
-            atool --list -- "${FILE}" || \
-            exit $?;;
-        rar)
-            ## Avoid password prompt by providing empty password
-            unrar lt -p- -- "${FILE}" || exit $?;;
-        7z)
-            ## Avoid password prompt by providing empty password
-            7z l -p -- "${FILE}" || exit $?;;
 
         ## OpenDocument
         # odt|ods|odp|sxw)
@@ -204,36 +177,43 @@ function handle_extension() {
         #     ## Uses: https://github.com/dilshod/xlsx2csv
         #     xlsx2csv -- "${FILE}" || exit $?;;
 
-        ## HTML
-        htm|html|xhtml)
-            ## Preview as text conversion
-            bat -l html -- "$FILE" || \
-            lynx -dump -- "${FILE}" || \
-            elinks -dump "${FILE}" || \
-            w3m -dump "${FILE}" || \
-            pandoc -s -t markdown -- "${FILE}" || exit $?;;
+   esac
+}
 
-        ## JSON
-        json)
-            jq --color-output . "${FILE}" || \
-            bat -l json "$FILE" || \
-            python -m json.tool -- "${FILE}" || exit $?;;
+function handle_archive() {
+    local ext=$1
+    case "${ext}" in
+        ## Archive
+        a|ace|alz|arc|arj|bz|bz2|cab|cpio|deb|gz|jar|lha|lz|lzh|lzma|lzo|\
+        rpm|rz|t7z|tar|tbz|tbz2|tgz|tlz|txz|tZ|tzo|war|xpi|xz|Z|zip)
+            bsdtar --list --file "${FILE}" || \
+            atool --list -- "${FILE}" || \
+            exit $?
+        ;;
 
+        rar)
+            ## Avoid password prompt by providing empty password
+            unrar lt -p- -- "${FILE}" || exit $?
+        ;;
 
+        7z)
+            ## Avoid password prompt by providing empty password
+            7z l -p -- "${FILE}" || exit $?
+        ;;
     esac
 }
 
 
 function handle_fallback() {
-    bat -- $FILE
+    bat --color=always --paging=never --plain -- $FILE || true
 }
 
 function start_preview(){
     [[ ! -d $CACHE_PATH ]] && mkdir -p $CACHE_PATH
 
-handle_image $MIMETYPE
-handle_extension $FILE_EXT
-handle_mime $MIMETYPE
+handle_media $FILE_EXT
+handle_documents $FILE_EXT
+handle_archive $FILE_EXT
 handle_fallback
 }
 
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -59,3 +59,8 @@ if [[ $ZDOTDIR/utilities.zsh ]];
 then
     source $ZDOTDIR/utilities.zsh
 fi
+
+if (($+commands[lf]));then
+    # enable lfcd
+    source $XDG_CONFIG_HOME/lf/lfcd.zsh
+fi
